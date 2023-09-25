f52582966d62fb9e83041891a95ca4e187179cc2
dennisloh95 <dennisloh95@gmail.com>
2023-09-25 18:06:33 +0800

    chore: add readme and do cleanup

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-// This Greeter contract will be interacted with through the ScrollMessenger across the bridge
 contract Greeter {
     string public greeting = "Hello World!";
 
-    // This function will be called by executeFunctionCrosschain on the Operator Smart Contract
     function setGreeting(string memory greeting_) public {
         greeting = greeting_;
     }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
 import dotenv from "dotenv";
@@ -7,37 +7,37 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  networks:{
-    mantle:{
-      url:process.env.MANTLE_MAINNET_RPC_URL,
-      accounts:[process.env.PRIVATE_KEY!],
+  networks: {
+    mantle: {
+      url: process.env.MANTLE_MAINNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!],
     },
-    mantleTestnet:{
-      url:process.env.MANTLE_TESTNET_RPC_URL,
-      accounts:[process.env.PRIVATE_KEY!],
-    }
+    mantleTestnet: {
+      url: process.env.MANTLE_TESTNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
   },
-  etherscan:{
-    apiKey:process.env.ETHERSCAN_API_KEY,
-    customChains:[
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
       {
-        network:"mantle",
-        chainId:5000,
-        urls:{
-          apiURL:`${process.env.MANTLE_MAINNET_EXPLORER}api`,
-          browserURL:process.env.MANTLE_MAINNET_EXPLORER!
-        }
+        network: "mantle",
+        chainId: Number(process.env.MANTLE_MAINNET_CHAIN_ID),
+        urls: {
+          apiURL: `${process.env.MANTLE_MAINNET_EXPLORER}api`,
+          browserURL: process.env.MANTLE_MAINNET_EXPLORER!,
+        },
       },
       {
-        network:"mantleTestnet",
-        chainId:5001,
-        urls:{
-          apiURL:`${process.env.MANTLE_TESTNET_EXPLORER}api`,
-          browserURL:process.env.MANTLE_TESTNET_EXPLORER!
-        }
-      }
-    ]
-  }
+        network: "mantleTestnet",
+        chainId: Number(process.env.MANTLE_TESTNET_CHAIN_ID),
+        urls: {
+          apiURL: `${process.env.MANTLE_TESTNET_EXPLORER}api`,
+          browserURL: process.env.MANTLE_TESTNET_EXPLORER!,
+        },
+      },
+    ],
+  },
 };
 
 export default config;
